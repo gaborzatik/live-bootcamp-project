c451ebeb64aa6cc0b87425f89d568318ad09ec9b
GZatik <gabor.zatik@gmail.com>
2025-01-06 15:20:31 +0100

    [Sprint 1][Task 4] feat: Structure auth service for testing

---
+++ auth-service/src/lib.rs
@@ -0,0 +1,33 @@
+use axum::{Router, routing::get};
+use std::error::Error;
+use axum::serve::Serve;
+
+// This struct encapsulates our application-related logic.
+pub struct Application {
+    server: Serve<Router, Router>,
+    // address is exposed as a public field
+    // so we have access to it in tests.
+    pub address: String,
+}
+
+impl Application {
+    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
+        // Define your router here.
+        let router = Router::new()
+            // Add necessary routes here later as the application grows.
+            .route("/", get(|| async { "Welcome to the Axum application!" }));
+
+        // Bind the provided address to a listener.
+        let listener = tokio::net::TcpListener::bind(address).await?;
+        let address = listener.local_addr()?.to_string();
+        let server = axum::serve(listener, router);
+
+        // Create a new Application instance and return it.
+        Ok(Self { server, address })
+    }
+
+    pub async fn run(self) -> Result<(), std::io::Error> {
+        println!("listening on {}", &self.address);
+        self.server.await
+    }
+}
